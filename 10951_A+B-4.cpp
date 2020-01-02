88e1c369c77d8bf08e1e6894e09e575d38782586
osiyeon <47052172+osiyeon@users.noreply.github.com>
2020-01-03 00:36:10 +0900

    Add files via upload
    while문을 사용하여 구현하기

---
+++ 10951_A+B-4.cpp
@@ -0,0 +1,18 @@
+/* 백준알고리즘
+ * 10951_A+B -4.cpp
+ *
+ *  Created on: 2020. 1. 2.
+ *      Author: 오시연
+ */
+
+#include<iostream>
+using namespace std;
+int main(){
+	int A, B=0;
+
+	while(cin>>A>>B){
+		cout<<A+B<<endl;;
+	}
+}
+
+
